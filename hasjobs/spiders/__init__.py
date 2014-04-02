5be3ea0d058437983a4f20077aad16e8278c6867
tanwairahul <tanwanirahul@gmail.com>
2014-04-02 16:33:30 +0530

    Adding a crawler for HasGeek

--- hasjobs/spiders/__init__.py
+++ hasjobs/spiders/__init__.py
@@ -2,3 +2,30 @@
 #
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
+from hasjobs.items import Job
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.selector import Selector
+
+
+class HasGeekSpider(CrawlSpider):
+    name = "hasjob"
+    allowed_domains = ["hasjob.co"]
+    start_urls = ["hasjob.co"]
+    rules = [Rule(SgmlLinkExtractor(allow=['/view/\w+']), 'parse_job')]
+
+    def parse_job(self, response):
+        '''
+            Parses the job information.
+        '''
+        sel = Selector(response)
+        job = Job()
+        job["source"] = response.url
+        job["title"] = sel.xpath("//h1/text()").extract()
+        job["postedDate"] = sel.xpath("//p[@class='post-date']/text()").extract()
+        job["location"] = sel.xpath("//span[@class='post-location']/text()").extract()
+        job["company"]["name"] = sel.xpath("//span[@class='post-company-name']/text()").extract()
+        job["company"]["url"] = sel.xpath("//span[@class='post-company-name']/text()").extract()
+        job["company"]["description"] = sel.xpath("//span[@class='post-company-name']/text()").extract()
+        job["description"] = sel.xpath("//span[@class='post-company-name']/text()").extract()
+        return job
